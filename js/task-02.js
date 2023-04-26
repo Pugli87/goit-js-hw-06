e2cc84941216343c52c2adca217b2d7fca2e6bd2
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-04-25 23:40:06 -0500

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,14 @@ const ingredients = [
 ];
 
 const ul = document.querySelector('#ingredients');
-
+// ciclo para insertar cada ingrediente dentro de la lista
 ingredients.forEach((ingredient) => {
+  //creamos y asignamos un ingrediente a la lista
   const li = document.createElement('li');
+  // asignamos el texto al ingrediente 
   li.textContent = ingredient;
+  // le asignamos la clase correspodiente al elemento de la lista
   li.classList.add('item');
-  ul.appendChild(li);
+  //incorporamos el elemento  al final del html
+  ul.append(li);
 });
