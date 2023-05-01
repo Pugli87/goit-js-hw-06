a5431d9a3e5b7176f35b96183ffb83af26acdb94
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-01 00:19:24 -0500

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const Input = document.querySelector(`#name-input`);
+
+const Output = document.querySelector('#name-output');
+
+Input.addEventListener('input', function() {
+  if (Input.value.trim() !== '') {
+    // se utiliza la function trim() para suprimir los espacios al inicio y al final 
+    Output.textContent = Input.value;
+  } else {
+    Output.textContent = 'Anonimo';
+  }
+});
