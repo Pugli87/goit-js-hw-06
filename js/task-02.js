824cf1de1446d6e74488dbf7d075ebc9ad00c2c7
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-04-30 22:49:11 -0500

    Tarea 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,21 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
-// const ulIngredients = document.getElementById("ingredients");
+ const ulIngredients = document.getElementById("ingredients");
 
-// ingredients.forEach(ingredient => {
-//   const li = document.createElement("li");
-//   li.classList.add("item");
-//   li.textContent = ingredient;
-//   ulIngredients.appendChild(li);
-// });
+ ingredients.forEach(ingredient => {
+   const li = document.createElement("li");
+   li.classList.add("item");
+   li.textContent = ingredient;
+   ulIngredients.appendChild(li);
+ });
 
 
-const ulIngredients = document.querySelector('#ingredients');
-const array = ingredients.map(ingredient => {
-  const li = document.createElement('li');
-  li.classList.add('item');
-  li.textContent = ingredient;
-  return li;
-});
-ulIngredients.append(...array);
