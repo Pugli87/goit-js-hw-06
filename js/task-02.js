5a01793cca8f1e2cbd47957d0d5afd34bed0adee
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-04-30 22:22:28 -0500

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredients = document.getElementById("ingredients");
+
+ingredients.forEach(ingredient => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ingredient;
+  ulIngredients.appendChild(li);
+});
