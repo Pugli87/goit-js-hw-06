9ea5ff0140529328d0f00e7b4f01215b3624009f
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-01 02:10:07 -0500

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
+    alert("Todos los espacios deben ser rellenados.");  
+  }
+
+  const object = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(object);
+  loginForm.reset();
+});
